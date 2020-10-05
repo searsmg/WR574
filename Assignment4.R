e8263b04d639c871edd378e10ff5c4e21ef2fa45
searsmg <megan.sears@colostate.edu>
2020-10-04 18:57:05 -0600

    10/4/20 set up assignment 4 DFs

---
+++ Assignment4.R
@@ -0,0 +1,70 @@
+#Assignment 4 for WR574 - Trace events, Precip undercatch, and probability
+
+rm(list=ls())
+
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(RColorBrewer)
+
+#set wd for saving plots
+setwd("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 4/")
+
+#Kalispell ASOS station hourly data. Mutate date to work with in R 
+Kal <- read.csv("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 3/Assignment3_KalASOS.csv")%>%
+  mutate(date.time = mdy_hm(date.time))
+
+Kal <-na.omit(Kal)
+
+#no trace included
+Kal_noT <- Kal[!(Kal$HourlyPrecip_in=="T"),]
+
+#trace included
+Kal_T <- read.csv("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 3/Kal_T.csv")%>%
+  mutate(date.time = mdy_hm(date.time))
+
+###########################################################################
+########################PLOT FORMATTING###################################
+
+#X-axis labels for monthly plots
+MonthLabels = c("Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug")
+
+#Height and width 
+PlotWidth = 15
+PlotHeight = 9
+
+# dont use this plot theme in future bc it was tweaked #
+PlotTheme = theme(axis.text=element_text(size=20),    #Text size for axis tick mark labels
+                  axis.title.x=element_text(size=24, hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),               #Text size and alignment for x-axis label
+                  axis.title.y=element_text(size=24, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              #Text size and alignment for y-axis label
+                  plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      #Text size and alignment for plot title
+                  legend.title=element_blank(),                                                                    #Text size of legend category labels
+                  legend.text=element_text(size=20),                                                                   #Text size of legend title
+                  legend.position = "bottom") 
+
+# a really long and annoying way to create minor tick axis tick marks
+every_nth <- function(x, nth, empty = TRUE, inverse = FALSE) 
+{
+  if (!inverse) {
+    if(empty) {
+      x[1:nth == 1] <- ""
+      x
+    } else {
+      x[1:nth != 1]
+    }
+  } else {
+    if(empty) {
+      x[1:nth != 1] <- ""
+      x
+    } else {
+      x[1:nth == 1]
+    }
+  }
+}
+
+##################################################################
+#Question 1 - cum sum of precip w/o trace and w/trace
+
+#cumsum WITHOUT TRACE
+
+
