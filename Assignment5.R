13ddd691891414b445749431e7f080fa5351b465
searsmg <megan.sears@colostate.edu>
2020-10-13 14:40:33 -0600

    10/13/20 just starting

---
+++ Assignment5.R
@@ -0,0 +1,11 @@
+rm(list=ls())
+
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(RColorBrewer)
+library(scales)
+
+#set wd for saving plots
+setwd("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 5/")
+
