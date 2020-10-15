97a9ee21dbde297798a0fab17b8a3bb1761de70a
searsmg <megan.sears@colostate.edu>
2020-10-15 16:34:52 -0600

    10/15/20 R studio is glitching so pushing this - still on Q1

--- Assignment5.R
+++ Assignment5.R
@@ -9,3 +9,47 @@ library(scales)
 #set wd for saving plots
 setwd("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 5/")
 
+#################################################################
+
+#plot formatting
+
+#X-axis labels for monthly plots
+MonthLabels = c("Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug")
+
+#Height and width 
+PlotWidth = 15
+PlotHeight = 9
+
+# dont use this plot theme in future bc it was tweaked #
+PlotTheme = theme(axis.text=element_text(size=20),    #Text size for axis tick mark labels
+                  axis.title.x=element_text(size=24, hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),               #Text size and alignment for x-axis label
+                  axis.title.y=element_text(size=24, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              #Text size and alignment for y-axis label
+                  plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      #Text size and alignment for plot title
+                  legend.title=element_blank(),                                                                    #Text size of legend category labels
+                  legend.text=element_text(size=20),                                                                   #Text size of legend title
+                  legend.position = "bottom") 
+
+# a really long and annoying way to create minor tick axis tick marks
+every_nth <- function(x, nth, empty = TRUE, inverse = FALSE) 
+{
+  if (!inverse) {
+    if(empty) {
+      x[1:nth == 1] <- ""
+      x
+    } else {
+      x[1:nth != 1]
+    }
+  } else {
+    if(empty) {
+      x[1:nth != 1] <- ""
+      x
+    } else {
+      x[1:nth == 1]
+    }
+  }
+}
+
+##################################################################
+#Question 1
+
+write.csv(Kal_Correct, "Kal_Correct.csv")
