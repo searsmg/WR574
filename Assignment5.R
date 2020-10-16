e6245edb6a9386fd821f444f1d2cd3b5b9150c9d
searsmg <megan.sears@colostate.edu>
2020-10-16 15:42:11 -0600

    10/16/20 finished Q3

--- Assignment5.R
+++ Assignment5.R
@@ -12,7 +12,10 @@ setwd("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 5/"
 
 load("~/Repos/WR574/Kal_Correct.RData")
 
-SNOTEL <- read.csv("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 5/SNOTEL_daily.csv")
+SNOTEL <- read.csv("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 5/SNOTEL_daily.csv")%>%
+  mutate(Date = mdy(Date))
+
+SNOTEL$Date <- as.POSIXct(SNOTEL$Date)
 
 #################################################################
 
@@ -25,14 +28,14 @@ MonthLabels = c("Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "
 PlotWidth = 15
 PlotHeight = 9
 
-# dont use this plot theme in future bc it was tweaked #
 PlotTheme = theme(axis.text=element_text(size=20),    #Text size for axis tick mark labels
                   axis.title.x=element_text(size=24, hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),               #Text size and alignment for x-axis label
                   axis.title.y=element_text(size=24, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              #Text size and alignment for y-axis label
                   plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      #Text size and alignment for plot title
                   legend.title=element_blank(),                                                                    #Text size of legend category labels
                   legend.text=element_text(size=20),                                                                   #Text size of legend title
-                  legend.position = "bottom") 
+                  legend.position = "bottom")
+
 
 # a really long and annoying way to create minor tick axis tick marks
 every_nth <- function(x, nth, empty = TRUE, inverse = FALSE) 
@@ -58,7 +61,7 @@ every_nth <- function(x, nth, empty = TRUE, inverse = FALSE)
 #Question 1 - this is very sloppy
 
 PLOT = "Air Temp and Cumulative Snow"
-q1 <- ggplot(Kal_Correct) + geom_line(aes(x=date.time, y=Precip_Snow_AirCum, colour="Temp"), size=1) + theme_classic() + geom_line(aes(x=date.time, y=AirTemp_C, colour="Temp"), size=1) + PlotTheme + labs(x="Water Year 2020", y="AirTemp (C)") + scale_y_continuous(sec.axis = sec_axis(~ . *3/300, name="Temp")) + scale_x_datetime(date_breaks = "1 month", labels = date_format("%b")) + scale_color_manual(values = c("Temp"= "blue", "Snow" = "green"))
+ggplot(Kal_Correct) + geom_line(aes(x=date.time, y=Precip_Snow_AirCum, colour="Snow"), size=1) + theme_classic() + geom_line(aes(x=date.time, y=AirTemp_C, colour="Temp"), size=1) + PlotTheme + labs(x="Water Year 2020", y="AirTemp (C)") + scale_x_datetime(date_breaks = "1 month", labels = date_format("%b")) + scale_color_manual(values = c("Temp"= "blue", "Snow" = "green"))
 
 ay <- list(
   tickfont = list(size=11.7),
@@ -79,7 +82,14 @@ ggplotly(q1) %>%
 
 #Question 3 - Steven said to do Q3 before Q2 and it'd be easier
 
+PLOT="Snow Depth"
+custombreaks1 <- seq(0, 4, .25)
+ggplot(SNOTEL) + geom_line(aes(x=Date, y=Snowfall_mm/1000, colour="Computed Snowfall"), size =1, group=1)+ geom_line(aes(x=Date, y=SnowDepthCum_mm/1000, colour="Observed Snow Depth"), size=1, group=1) + scale_y_continuous(breaks = custombreaks1, labels = every_nth(custombreaks1, 2, inverse=TRUE)) + labs(x="Date", y="Snowfall/Snow Depth (m)") + scale_color_manual(values = c("Computed Snowfall"= "purple", "Observed Snow Depth" = "blue")) + theme_classic()+ PlotTheme + scale_x_date(date_labels = "%b %Y", date_breaks = "2 month", limits=as.Date(c("2019-09-01", "2020-08-31")))
+
+ggsave(paste(PLOT,".png",sep=""), width = PlotWidth, height = PlotHeight)
 
 
+#######################################################################
+#Question 2
 
-ggplot(SNOTEL) + geom_line(aes(x=Date, y=SWE_mm, colour="SWE"), group=1)+ geom_line(aes(x=Date, y=SnowDepth_mm, colour="Snow Depth"), group=1)+ geom_line(aes(x=Date, y=Precip_mm, colour="Precip"),group=1)
+
