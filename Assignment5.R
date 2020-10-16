f3779a2a9afffc63e296db8ea621a5753b0bfe82
searsmg <megan.sears@colostate.edu>
2020-10-15 18:10:00 -0600

    10/15/20 working on q3

--- Assignment5.R
+++ Assignment5.R
@@ -5,9 +5,14 @@ library(dplyr)
 library(lubridate)
 library(RColorBrewer)
 library(scales)
+library(plotly)
 
 #set wd for saving plots
 setwd("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 5/")
+
+load("~/Repos/WR574/Kal_Correct.RData")
+
+SNOTEL <- read.csv("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 5/SNOTEL_daily.csv")
 
 #################################################################
 
@@ -50,6 +55,31 @@ every_nth <- function(x, nth, empty = TRUE, inverse = FALSE)
 }
 
 ##################################################################
-#Question 1
+#Question 1 - this is very sloppy
 
-write.csv(Kal_Correct, "Kal_Correct.csv")
+PLOT = "Air Temp and Cumulative Snow"
+q1 <- ggplot(Kal_Correct) + geom_line(aes(x=date.time, y=Precip_Snow_AirCum, colour="Temp"), size=1) + theme_classic() + geom_line(aes(x=date.time, y=AirTemp_C, colour="Temp"), size=1) + PlotTheme + labs(x="Water Year 2020", y="AirTemp (C)") + scale_y_continuous(sec.axis = sec_axis(~ . *3/300, name="Temp")) + scale_x_datetime(date_breaks = "1 month", labels = date_format("%b")) + scale_color_manual(values = c("Temp"= "blue", "Snow" = "green"))
+
+ay <- list(
+  tickfont = list(size=11.7),
+  titlefont=list(size=14.6),
+  overlaying = "y",
+  nticks = 5,
+  side = "right",
+  title = "Second y axis"
+)
+
+
+ggplotly(q1) %>%
+  add_lines(x=~date.time, y=~AirTemp_C, colors=NULL, yaxis="y2", 
+            data=Kal_Correct, showlegend=FALSE, inherit=FALSE) %>%
+  layout(yaxis2 = ay)
+
+###################################################################
+
+#Question 3 - Steven said to do Q3 before Q2 and it'd be easier
+
+
+
+
+ggplot(SNOTEL) + geom_line(aes(x=Date, y=SWE_mm, colour="SWE"), group=1)+ geom_line(aes(x=Date, y=SnowDepth_mm, colour="Snow Depth"), group=1)+ geom_line(aes(x=Date, y=Precip_mm, colour="Precip"),group=1)
