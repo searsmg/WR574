fb81a104c19eaab032f3265aebbafb3a5ec784db
searsmg <megan.sears@colostate.edu>
2020-11-08 14:15:34 -0700

    11/8/20 set up plot formatting and wd

--- Assignment8.R
+++ Assignment8.R
@@ -1,2 +1,63 @@
 # WR574 Assignment 8 Sublimation 
 
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+
+rm(list=ls())
+
+#load("~/Repos/WR574/Assignment4data.RData")
+
+setwd("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 8/")
+
+##################################################################
+##################################################################
+
+#Plot formatting
+#Plot Format
+
+#X-axis labels for plots that are monthly
+MonthLabels = c("Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug")
+
+#Plot size
+PlotWidth = 15
+PlotHeight = 9
+
+# use this plot format on remaining assignments - tweaked to remove grid lines and gray background
+PlotFormat = theme(axis.text=element_text(size=20),
+                   axis.title.x=element_text(size=24, hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),              
+                   axis.title.y=element_text(size=24, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              
+                   plot.title=element_text(size=26,face="bold",hjust=0.5, margin=margin(t=20, r=20, b=20, l=20)),      
+                   legend.title=element_blank(),                                                                    
+                   legend.text=element_text(size=20),                                                                   
+                   legend.position = "bottom", 
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   axis.line = element_line(colour = "black"))
+
+
+# a really long way to create minor tick axis tick marks
+every_nth <- function(x, nth, empty = TRUE, inverse = FALSE) 
+{
+  if (!inverse) {
+    if(empty) {
+      x[1:nth == 1] <- ""
+      x
+    } else {
+      x[1:nth != 1]
+    }
+  } else {
+    if(empty) {
+      x[1:nth != 1] <- ""
+      x
+    } else {
+      x[1:nth == 1]
+    }
+  }
+}
+
+##################################################################
+##################################################################
+
+# Question 1
