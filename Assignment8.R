9925c20d28213b7fa4485cb8f542b17c3767ceb1
searsmg <megan.sears@colostate.edu>
2020-11-08 19:46:18 -0700

    11/8/20 calcs for q2 complete

--- Assignment8.R
+++ Assignment8.R
@@ -3,6 +3,8 @@
 library(ggplot2)
 library(dplyr)
 library(lubridate)
+library(waldo)
+libary(plyr)
 
 rm(list=ls())
 
@@ -10,11 +12,38 @@ rm(list=ls())
 
 setwd("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 8/")
 
+#making sure all the vars needed are here
+#GetUz <- Kal_T %>%
+#  select(date.time, Uz)
+
+#GetRH <- Kal %>%
+#  select(date.time, RH_.)
+
+#rm(list= ls()[!(ls() %in% c('GetRH','GetUz', 'Kal_Correct'))])
+
+#SeaLevelPress <- read.csv("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 8/SeaLevelPress.csv")%>%
+#  mutate(date.time = mdy_hm(date.time))
+
+#create Rdata file specific for this assignment
+#save.image(file="Kal8.Rdata") #commented out now since the file is created
+
+#Kal8 <- merge(Kal_Correct, GetRH, by="date.time")
+#Kal8 <- merge(Kal8, GetUz, by="date.time")
+#Kal8 <- merge(Kal8, SeaLevelPress, by="date.time")
+
+#Kal8 <- Kal8 %>%
+#  select(date.time, AirTemp_C, PrecipCorr, Precip_Snow_Dew, RH_., Uz, -SeaLevelPress_mB)
+
+#save again - ready for calcs now
+#save.image(file="Kal8.Rdata")
+
+Kal8_new <- read.csv("C:/Users/sears/Documents/4_Classes_FA20/WR 574/Assignments/Assignment 8/Kal8_update.csv")%>%
+  mutate(date.time = mdy_hm(date.time))
+
 ##################################################################
 ##################################################################
 
 #Plot formatting
-#Plot Format
 
 #X-axis labels for plots that are monthly
 MonthLabels = c("Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug")
@@ -60,4 +89,17 @@ every_nth <- function(x, nth, empty = TRUE, inverse = FALSE)
 ##################################################################
 ##################################################################
 
-# Question 1
+# Question 2 - snowpack sublimation
+
+kconstant <- -11.3
+
+#find K
+Kal8_new <- Kal8_new %>%
+  mutate(KalPress_mB = SeaLevelPress_mB*exp(-0.00012*908)) %>%
+  mutate(AirDens = (0.34722*KalPress_mB)/(AirTemp_C + 273.15)) %>%
+  mutate(esat = (6.112*exp((17.62*AirTemp_C)/(243.12+AirTemp_C)))) %>%
+  mutate(ea = (RH_. * esat)/100) %>% 
+  mutate(sublim = if_else(AirTemp_C <= 0, kconstant * (AirDens/KalPress_mB)*Uz*(ea-esat), 0)) %>%
+  mutate(sublim_cum = cumsum(sublim))
+
+
